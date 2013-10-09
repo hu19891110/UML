cf4be1f419784e9cba07ff3b5d9d1ab2c5603d2c
Prikkel <Thomasprikkel@gmail.com>
2013-10-09 13:50:22 +0200

    Login system added
    added Flexi-auth ACL to codeIgniter framework

--- application/config/config.php
+++ application/config/config.php
@@ -26,7 +26,7 @@ $config['base_url']	= '';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
@@ -248,7 +248,7 @@ $config['sess_cookie_name']		= 'ci_session';
 $config['sess_expiration']		= 7200;
 $config['sess_expire_on_close']	= FALSE;
 $config['sess_encrypt_cookie']	= FALSE;
-$config['sess_use_database']	= FALSE;
+$config['sess_use_database']	= TRUE;
 $config['sess_table_name']		= 'ci_sessions';
 $config['sess_match_ip']		= FALSE;
 $config['sess_match_useragent']	= TRUE;
@@ -357,6 +357,18 @@ $config['rewrite_short_tags'] = FALSE;
 */
 $config['proxy_ips'] = '';
 
+/*
+|--------------------------------------------------------------------------
+| Static salt
+|--------------------------------------------------------------------------
+|
+|
+*/
+
+
+$config['security']['static_salt'] = 'UMLcheckerisbestwelgeniaal';
+
 
 /* End of file config.php */
 /* Location: ./application/config/config.php */
+
