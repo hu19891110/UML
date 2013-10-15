1fa3aa7d7683d9bff5ff502e0187b86e1bc8c3e3
Prikkel <Thomasprikkel@gmail.com>
2013-10-15 13:28:25 +0200

    Upload permissions and template.

--- application/controllers/upload.php
+++ application/controllers/upload.php
@@ -5,12 +5,45 @@ class Upload extends CI_Controller {
 	function __construct()
 	{
 		parent::__construct();
+		
+		$this->load->database();
+		$this->load->library('session');
 		$this->load->helper(array('form', 'url'));
+		
+		$this->login = new stdClass;
+		
+		$this->load->library('flexi_auth');	
+		
+		if (! $this->flexi_auth->is_logged_in_via_password()) 
+		{
+			
+			$this->flexi_auth->set_error_message('You must be logged in to access this area.', TRUE);
+			$this->session->set_flashdata('message', $this->flexi_auth->get_messages());
+			redirect('login');
+		}
+		
+		$this->load->vars('base_url', 'http://localhost:8888/');
+		$this->load->vars('includes_dir', 'http://localhost:8888/includes/');
+		$this->load->vars('current_url', $this->uri->uri_to_assoc(1));
+		
+		$this->data = null;		
+		
+		$currentuser_id = $this->flexi_auth->get_user_id();
+		$sql_where = array($this->flexi_auth->db_column('user_acc', 'id') => $currentuser_id);
+		$this->data['currentuser'] = $this->flexi_auth->get_users_row_array(FALSE, $sql_where);
 	}
 
 	function index()
 	{
-		$this->load->view('upload_form', array('error' => ' ' ));
+		$this->data['error'] = ' ';
+		$data['maincontent'] = $this->load->view('upload_form', $this->data, TRUE);
+		
+		if ($this->flexi_auth->is_admin()) {
+			$this->load->view('template-teacher', $data);
+		} else {
+			$this->load->view('template-student', $data);
+		}
+		
 	}
 
 	function do_upload()
@@ -26,7 +59,7 @@ class Upload extends CI_Controller {
 		if ( ! $this->upload->do_upload())
 		{
 			$error = array('error' => $this->upload->display_errors());
-
+			
 			$this->load->view('upload_form', $error);
 		}
 		else
