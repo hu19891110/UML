9fab31cb62230f8742d1683f96c3439097ee3e05
Prikkel <Thomasprikkel@gmail.com>
2013-10-15 12:27:12 +0200

    Navigatie dynamisch gemaakt
    Gebruik gemaakt van base_url var.

--- application/views/includes/header-teacher.php
+++ application/views/includes/header-teacher.php
@@ -26,7 +26,7 @@
   			<ul class="title-area">
    			 <!-- Title Area -->
     		<li class="name">
-      	<h1><a href="#">UML Checker </a></h1>
+      	<h1><a href="<?php echo $base_url; ?>">UML Checker </a></h1>
     		</li>
    		 <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
    		 <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
@@ -36,7 +36,7 @@
     <!-- Left Nav Section -->
     <ul class="left">
       <li class="divider"></li>
-      <li class="active"><a href="#">Dashboard</a></li>
+      <li class="active"><a href="<?php echo $base_url; ?>">Dashboard</a></li>
       <li class="divider"></li>
       <li><a href="#">Assignments</a></li>
       <li class="divider"></li>
