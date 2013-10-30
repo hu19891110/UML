a2005567da1c9ceeb8fb56a8bc3558cba95ba3fa
Prikkel <Thomasprikkel@gmail.com>
2013-10-30 18:08:58 +0100

    Die merge branch was vaag.

--- application/views/includes/head.php
+++ application/views/includes/head.php
@@ -15,4 +15,4 @@
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>
 	<script type="text/javascript" src="<?php echo $includes_dir;?>Foundation/tables/responsive-tables.js"></script>
 	<script src="<?php echo $includes_dir;?>Foundation/js/vendor/custom.modernizr.js"></script>
-		
+	
