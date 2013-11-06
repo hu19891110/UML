c3652206d5a1038d82c6b300f7478b480794ab37
RachelleBlok <rachelle@Rachelles-MacBook-Air.local>
2013-11-06 21:12:44 +0100

    deadline view

--- application/views/manage_deadlines_view.php
+++ application/views/manage_deadlines_view.php
@@ -6,108 +6,9 @@
 		</div>
 	<?php } ?>
 	
-				
-	<div id="mainwrapper">	
-	<!--	
-		<div id="box-6" class="box">
-		<img id="image-6" src="../includes/images/assignments.jpg"/>
-		<a href="<?php echo site_url('assignments') ?>"><span class="caption scale-caption">
-			<h3></h3>
-			<p> Upload test answers <br/>
-			Get an overview of uploaded assignments <br/>
-			Mark assignments as checked</p>
-		</span></a>
-		</div>
-		
-			<div id="box-6" class="box">
-		<img id="image-6" src="../includes/images/deadlines.jpg"/>
-		<span class="caption scale-caption">
-			<h3></h3>
-			<p> </p>
-		</span>
-		</div>
-		
-			<div id="box-6" class="box">
-		<img id="image-6" src="../includes/images/archive.jpg"/>
-		<span class="caption scale-caption">
-			<h3></h3>
-			<p> </p>
-		</span>
-		</div>
-		
-			<div id="box-6" class="box">
-		<img id="image-6" src="../includes/images/register.jpg"/>
-		<a href="<?php echo site_url('register') ?>"><span class="caption scale-caption">
-			<h3></h3>
-			<p> </p>
-		</span></a>
-		</div>
-		
-			<div id="box-6" class="box">
-		<img id="image-6" src="../includes/images/userdetails.jpg"/>
-		<a href="<?php echo $base_url;?>dashboard/update_user_account/<?php echo $currentuser['uacc_id']; ?>">
-		<span class="caption scale-caption">
-			<h3></h3>
-			<p> </p>
-		</span>
-		</div> -->
-	
-			
-		<div class="row">			
-				<?php echo form_open(current_url()); ?>
-				<table id="asstocheck" class="large-4 columns margin-left">
-  				<thead>
-    				<tr>
-      			<th colspan="3" style="text-align: center;">Upcoming deadlines</th>
-    				</tr>
-  				</thead>
-  				<tbody>
-  				<tr>
-      			<th>Deadline</th>
-      			<th>Date of deadline</th>
-      			<th>Delete deadline</th>
-    			</tr>
-      			<?php 
-      			foreach ($deadlines as $deadline)
-      			{ ?>
-      			<tr>
-	      			<td><input type="hidden" name="delete[<?php echo $deadline[$this->flexi_auth->db_column('deadline', 'id')];?>][id]" value="<?php echo $deadline[$this->flexi_auth->db_column('deadline', 'id')];?>"/>
-	      			<a href="<?php echo $base_url . 'dashboard/deadline/'. $deadline[$this->flexi_auth->db_column('deadline', 'id')];?>"><?php echo $deadline[$this->flexi_auth->db_column('deadline', 'desc')];?></a></td>
-	      			<td><?php echo $deadline[$this->flexi_auth->db_column('deadline', 'enddate')];?></td>
-	      			<td>
-	      			<?php 
-						// Define form input values.
-						$current_status = 0; 
-						$new_status = NULL;
-					?>
-	      			
-	      			<input type="hidden" name="delete[<?php echo $deadline[$this->flexi_auth->db_column('deadline', 'id')];?>][current_status]" value="<?php echo $current_status ?>"/>
-					<input type="hidden" name="delete[<?php echo $deadline[$this->flexi_auth->db_column('deadline', 'id')];?>][new_status]" value="0"/>
-											
-					<input style="float:right;" type="checkbox" name="delete[<?php echo $deadline[$this->flexi_auth->db_column('deadline', 'id')];?>][new_status]" value="1" <?php echo $new_status ?>/></td>
-	      		</tr>
-	      		
-      			<?php } ?>		
-      			</tbody>
-      			<tfoot>
-      				<tr><td colspan="3">
-      				<input type="submit" name="delete_deadline" id="add_deadline" value="Delete Checked deadlines" class="small button"/>
-      				</td></tr>
-      			</tfoot>
-				</table>
-				<?php echo form_close(); ?>
-				<div class="large-3 columns margin-left deadline">
-					<h3> Add a new deadline </h3>
-<!-- Main Content -->
-	<div class="large-12 columns">
-				<h2>Manage deadlines</h2> 
+		<h2>Manage deadlines</h2> 
 
-			<?php if (! empty($message)) { ?>
-				<div id="message">
-					<?php echo $message; ?>
-				</div>
-			<?php } ?>
-
+		
 		<div class="large-6 columns">
 			<h3> Add a new deadline </h3>
 					<?php
@@ -191,11 +92,50 @@
 					<?php echo form_close();?>
 					
 
-				</div><!--large-3 columns -->
+		</div><!--large-6 columns -->
 										
-			
-				</div><!-- end large 12 columns -->
 
-			</div> <!-- mainwrapper --> 	
-				
-		</div> <!-- end 12 columns --> 		
+				<table id="asstocheck" class="large-5 columns" style="max-width:800px;">
+  				<thead>
+    				<tr>
+      			<th colspan="3" style="text-align: center;">Upcoming deadlines</th>
+    				</tr>
+  				</thead>
+  				<tbody>
+  				<tr>
+      			<th>Deadline</th>
+      			<th>Date of deadline</th>
+      			<th>Delete deadline</th>
+    			</tr>
+      			<?php 
+      			foreach ($deadlines as $deadline)
+      			{ ?>
+      			<tr>
+	      			<td><input type="hidden" name="delete[<?php echo $deadline[$this->flexi_auth->db_column('deadline', 'id')];?>][id]" value="<?php echo $deadline[$this->flexi_auth->db_column('deadline', 'id')];?>"/>
+	      			<a href="<?php echo $base_url . 'dashboard/deadline/'. $deadline[$this->flexi_auth->db_column('deadline', 'id')];?>"><?php echo $deadline[$this->flexi_auth->db_column('deadline', 'desc')];?></a></td>
+	      			<td><?php echo $deadline[$this->flexi_auth->db_column('deadline', 'enddate')];?></td>
+	      			<td>
+	      			<?php 
+						// Define form input values.
+						$current_status = 0; 
+						$new_status = NULL;
+					?>
+	      			
+	      			<input type="hidden" name="delete[<?php echo $deadline[$this->flexi_auth->db_column('deadline', 'id')];?>][current_status]" value="<?php echo $current_status ?>"/>
+					<input type="hidden" name="delete[<?php echo $deadline[$this->flexi_auth->db_column('deadline', 'id')];?>][new_status]" value="0"/>
+											
+					<input style="float:right;" type="checkbox" name="delete[<?php echo $deadline[$this->flexi_auth->db_column('deadline', 'id')];?>][new_status]" value="1" <?php echo $new_status ?>/></td>
+	      		</tr>
+	      		
+      			<?php } ?>		
+      			</tbody>
+      			<tfoot>
+      				<tr><td colspan="3">
+      				<input type="submit" name="delete_deadline" id="add_deadline" value="Delete Checked deadlines" class="small button"/>
+      				</td></tr>
+      			</tfoot>
+				</table>
+			
+
+	</div><!-- end large 12 columns -->
+	
