10a59e45b8db0ef371add0a84741093f9b0d4b2e
Prikkel <Thomasprikkel@gmail.com>
2013-11-14 12:12:32 +0100

    Created error displaying view.

--- application/views/checked_assignments_per_student_view.php
+++ application/views/checked_assignments_per_student_view.php
@@ -6,7 +6,7 @@
 		<span style="clear:both; float:right;"> Alles goedkeuren | Alles afkeuren </span>
 	</h4>
 	</div> <!-- end h2bg --> 
-	
+	<!--
 	<p> <b> Line 15:</b> The name of the class should be 'student' in stead of 'students' <br/>
 	<small> For this mistake 1 point is substracted </small>
 	</p>
@@ -18,12 +18,78 @@
 	<p> <b> Line 20:</b> The method of the class Mario should be 'getDirection(position)' in stead of 'getDirection()' <br/>
 	<small> For this mistake 2 points are substracted </small>
 	</p>
-	
+	-->
 	<? 
 		foreach ($errors as $error) {
 			
-			echo $error[$this->flexi_auth->db_column('checker_error', 'error_id')];
-			
+			 $error_id = $error[$this->flexi_auth->db_column('checker_error', 'error_id')];
+			 if ($error_id == 1) {
+				 ?>
+				 <p>Relatie '<?php echo $error[$this->flexi_auth->db_column('checker_error', 'relatie')];?>': De relatie komt wel voor in het ingeleverde model maar heeft een <strong>andere</strong> naam.</p>
+				 <?php
+			 } else if ($error_id == 2) {
+				 ?>
+				 <p>Relatie '<?php echo $error[$this->flexi_auth->db_column('checker_error', 'relatie')];?>': Deze relatie heeft NIET dezelfde <strong>beginbestemming.</strong> </p>
+				 <?php
+			 } else if ($error_id == 3) {
+				 ?>
+				 <p>Relatie '<?php echo $error[$this->flexi_auth->db_column('checker_error', 'relatie')];?>': Deze relatie heeft NIET dezelfde <strong>eindbestemming.</strong> </p>
+				 <?php
+			 } else if ($error_id == 4) {
+				 ?>
+				 <p>Relatie '<?php echo $error[$this->flexi_auth->db_column('checker_error', 'relatie')];?>': De relatie is NIET van dezelfde soort. </p>
+				 <?php
+			 } else if ($error_id == 5) {
+				 ?>
+				 <p> Multiplicty error</p>
+				 <?php
+			 } else if ($error_id == 6) {
+				 ?>
+				 <p>Klasse '<?php echo $error[$this->flexi_auth->db_column('checker_error', 'class')];?>', attribuut '<?php echo $error[$this->flexi_auth->db_column('checker_error', 'attribute')];?>': Het Datatype (<?php echo $error[$this->flexi_auth->db_column('checker_error', 'datatype')];?>) komt niet overeen. </p>
+				 <?php
+			 } else if ($error_id == 7) {
+				 ?>
+				 
+				 <?php
+			 } else if ($error_id == 8) {
+				 ?>
+				 
+				 <?php
+			 } else if ($error_id == 9) {
+				 ?>
+				 
+				 <?php
+			 } else if ($error_id == 10) {
+				 ?>
+				 <p>Klasse '<?php echo $error[$this->flexi_auth->db_column('checker_error', 'class')];?>': Het attribuut '<?php echo $error[$this->flexi_auth->db_column('checker_error', 'attribute')];?>' mist in het ingeleverde model.</p>
+
+				 <?php
+			 } else if ($error_id == 11) {
+				 ?>
+				 
+				 <?php
+			 } else if ($error_id == 12) {
+				 ?>
+				 
+				 <?php
+			 } else if ($error_id == 13) {
+				 ?>
+				 
+				 <?php
+			 } else if ($error_id == 14) {
+				 ?>
+				 
+				 <?php
+			 } else if ($error_id == 15) {
+				 ?>
+				 <p>Klasse '<?php echo $error[$this->flexi_auth->db_column('checker_error', 'class')];?>' mist in het ingeleverde model. </p>
+				 <?php
+			 } else if ($error_id == 16) {
+				 ?>
+				 
+				 <?php
+			}
+			echo '<br/>';
 		}
 		
 	?>	
