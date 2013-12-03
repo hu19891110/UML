a45437b31c1bf3113e270a251d534b6ac8050e07
tjebb <anakinluke@gmail.com>
2013-12-03 15:49:23 +0100

    Forgot password viewsssssss.

---
+++ application/views/forgot_password_update_view.php
@@ -0,0 +1,50 @@
+<html>
+<head>
+	<meta name="robots" content="index, follow"/>
+	<meta name="designer" content="Five-IT"/> 
+	<meta name="copyright" content="Copyright <?php echo date('Y');?> Five-IT, All rights Reserved"/>
+	<meta http-equiv="imagetoolbar" content="no"/>	
+	
+	<link rel="stylesheet" href="<?php echo $includes_dir;?>css/login.css?v=1.0">
+</head>
+	
+<body>	
+	<!-- Main Content -->
+			<div id="loginbox">
+				<div id="titlebox"><h2>Forgot password</h2></div>
+	<?php if (! empty($message)) { ?>
+				<div id="message">
+					<?php echo $message; ?>
+				</div>
+			<?php } ?>
+				
+				<?php echo form_open(current_url());	?>  	
+					<div class="w100 frame">
+						<ul>
+							<li>
+								<small>
+									<strong>For this demo, the following validation settings have been defined:</strong><br/>
+									Password length must be more than <?php echo $this->flexi_auth->min_password_length(); ?> characters in length.<br/>Only alpha-numeric, dashes, underscores, periods and comma characters are allowed.
+								</small>
+							</li>
+							<li class="info_req">
+								<label for="new_password">New Password:</label>
+								<input type="password" id="new_password" name="new_password" value=""/>
+							</li>
+							<li class="info_req">
+								<label for="confirm_new_password">Confirm New Password:</label>
+								<input type="password" id="confirm_new_password" name="confirm_new_password" value=""/>
+							</li>
+							<li class="info_req">
+								<label for="submit">Change Password:</label>
+								<input type="submit" name="change_forgotten_password" id="submit" value="Submit" class="link_button large"/>
+							</li>
+						</ul>
+					</div>
+				<?php echo form_close();?>
+			
+	</div> <!-- einde loginbox --> 
+<body>
+</html>	
+
+
---
+++ application/views/forgot_password_view.php
@@ -0,0 +1,41 @@
+<html>
+<head>
+	<meta name="robots" content="index, follow"/>
+	<meta name="designer" content="Five-IT"/> 
+	<meta name="copyright" content="Copyright <?php echo date('Y');?> Five-IT, All rights Reserved"/>
+	<meta http-equiv="imagetoolbar" content="no"/>	
+	
+	<link rel="stylesheet" href="<?php echo $includes_dir;?>css/login.css?v=1.0">
+</head>
+	
+<body>	
+	<!-- Main Content -->
+			<div id="loginbox">
+				<div id="titlebox"><h2>Forgot password</h2></div>
+				<?php if (! empty($message)) { ?>
+				<div id="message">
+					<?php echo $message; ?>
+				</div>
+			<?php } ?>
+				
+				<?php echo form_open(current_url());	?>  	
+					<div class="w100 frame">
+						<ul>
+							<li class="info_req">
+								<label for="identity">Email or Username:</label>
+								<input type="text" id="identity" name="forgot_password_identity" value="" class="tooltip_trigger"
+									title="Please enter either your email address or username defined during registration."
+								/>
+							</li>
+							<li>
+								<label for="submit">Send Email:</label>
+								<input type="submit" name="send_forgotten_password" id="submit" value="Submit" class="link_button large"/>
+								<small>Note: By default, this demo is set so that the password must be reset within 15 minutes of the 'forgotten password' email being sent.</small>
+							</li>
+						</ul>
+					</div>	
+				<?php echo form_close();?>
+	</div> <!-- einde loginbox --> 
+<body>
+</html>	
+
