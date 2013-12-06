e005f5484d057a8c953c1efe8bba07f0d72f4f64
RachelleBlok <rachelle@Rachelles-MacBook-Air.local>
2013-12-06 21:10:04 +0100

    naampje weg

--- application/views/includes/header-student.php
+++ application/views/includes/header-student.php
@@ -26,7 +26,7 @@
   			<ul class="title-area">
    			 <!-- Title Area -->
     		<li class="name">
-      	<h1><a href="<?php echo $base_url; ?>">UML Checker </a></h1>
+      	<h1><a href="<?php echo $base_url; ?>"> </a></h1>
       		</li>
    		     <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
   			</ul>
