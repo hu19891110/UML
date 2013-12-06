0459a373c6fc6ab06c3866748f22de1729b3445e
Prikkel <Thomasprikkel@gmail.com>
2013-12-06 20:57:28 +0100

    navigatie fix

--- application/views/includes/header-teacher.php
+++ application/views/includes/header-teacher.php
@@ -23,14 +23,6 @@
 <div id="container">	
 	<div class="row">
 		<nav class="top-bar">
-  			<ul class="title-area">
-   			 <!-- Title Area -->
-    		<li class="menutitel name">
-      	<h1><a href="#"> UML Checker </a></h1>
-    		</li>
-   		 <!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
-   		 <li class="toggle-topbar menu-icon"><a href="#"><span>Menu</span></a></li>
-  			</ul>
 
 		<section class="top-bar-section">
     		<!-- Left Nav Section -->
